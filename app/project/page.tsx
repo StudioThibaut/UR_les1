248d67e1e10f65d66bc63afee319518fc65f1792
ThibautVandenEynden <thivdey@gmail.com>
2026-02-02 11:36:38 +0100

    Add files via upload

---
+++ app/project/page.tsx
@@ -0,0 +1,17 @@
+export default function AboutPage() {
+  return (
+    <div className="max-w-4xl mx-auto space-y-6">
+      <h1 className="text-3xl font-bold">About Us</h1>
+      <p className="text-gray-700">
+        We are a company dedicated to providing the best solutions for your needs. 
+        Our team is passionate about creating modern, scalable, and user-friendly applications.
+      </p>
+      <ul className="list-disc pl-5 text-gray-700 space-y-2">
+        <li>Innovative technology</li>
+        <li>Professional team</li>
+        <li>Customer satisfaction</li>
+      </ul>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate ullam facere alias doloremque suscipit corporis excepturi odio assumenda cum eveniet esse possimus, illo perspiciatis corrupti molestias dolore. Necessitatibus odio alias ad non, velit, aperiam consectetur praesentium expedita quod ratione facilis nesciunt officiis nam incidunt id reprehenderit temporibus ab enim deleniti.</p>
+    </div>
+  )
+}
